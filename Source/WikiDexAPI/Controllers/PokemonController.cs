1c09453d860caa5dc6fb97b78ea4ce92067d1372
Gonzalo Hidalgo <ghidalgo@frostdisk.com>
2021-05-31 20:18:57 -0400

    Empty basic project for now

---
+++ Source/WikiDexAPI/Controllers/PokemonController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using WikiDexAPI.Model;
+
+namespace WikiDexAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PokemonController>/5
+        [HttpGet("ByNatNumber/{id}")]
+        public PokemonDataModel GetByNatNumber(int id)
+        {
+            // This is temporary
+            return new PokemonDataModel()
+            {
+                NationalNumber = 25,
+                Name = "Pikachu",
+                Description = "Pikachu es un Pokémon de tipo eléctrico introducido en la primera generación.",
+                Types = new[] { "Eléctrico" }
+            };
+        }
+    }
+}
---
+++ Source/WikiDexAPI/Model/PokemonDataModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WikiDexAPI.Model
+{
+    public class PokemonDataModel
+    {
+        public int NationalNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string[] Types { get; set; }
+    }
+}
